cc6b85b45197fa2531cc2d22c7ba7ba51dc9de23
jiejietop <1161959934@qq.com>
2020-04-07 15:15:49 +0800

    add install-zsh.sh...

--- install/install-zsh.sh
+++ install/install-zsh.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
+path=$(cd `dirname $0`; pwd)
+
 sudo apt-get install -y zsh git wget
 
 # 从git上把oh-my-zsh clone下来到根目录下
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone https://gitee.com/jiejieTop/ohmyzsh.git ~/.oh-my-zsh
 # 再在根目录下copy一份.zshrc配置
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+# cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+# wget https://raw.githubusercontent.com/jiejieTop/linux-script-tools/master/install/.zshrc ~/.zshrc
+
+cp $path/.zshrc ~/.zshrc
+
+source ~/.zshrc
 
 # 设置zsh是默认shell
-chsh -s /bin/zsh
+# chsh -s /bin/zsh
 
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
@@ -17,6 +24,3 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/paulirish/git-open.git $ZSH_CUSTOM/plugins/git-open
 
 source ~/.zshrc
-
-
-
