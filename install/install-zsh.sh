22bfbad0781057ead09f6bd598bc76a04b1d3db1
jiejie <1161959934@qq.com>
2020-04-08 06:39:27 +0000

    update zsh...

--- install/install-zsh.sh
+++ install/install-zsh.sh
@@ -1,26 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-path=$(cd `dirname $0`; pwd)
+sudo apt-get -y install git wget zsh
 
-sudo apt-get install -y zsh git wget
-
-# 从git上把oh-my-zsh clone下来到根目录下
-git clone https://gitee.com/jiejieTop/ohmyzsh.git ~/.oh-my-zsh
-# 再在根目录下copy一份.zshrc配置
-# cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-# wget https://raw.githubusercontent.com/jiejieTop/linux-script-tools/master/install/.zshrc ~/.zshrc
-
-cp $path/.zshrc ~/.zshrc
-
-source ~/.zshrc
-
-# 设置zsh是默认shell
-# chsh -s /bin/zsh
-
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+if [ ! -d ~/.oh-my-zsh -o ! -f ~/.zshrc ]; then
+    sh -c "$(wget https://gitee.com/jiejieTop/ohmyzsh/raw/master/tools/install.sh -O -)"
+fi
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-git clone https://github.com/paulirish/git-open.git $ZSH_CUSTOM/plugins/git-open
+git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-source ~/.zshrc
+git clone https://github.com/paulirish/git-open.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/git-open
+
+sed -i 's/plugins=(git)/plugins=(z git sudo extract vi-mode git-open safe-paste command-not-found colored-man-pages zsh-syntax-highlighting)/g' ~/.zshrc
+
+# source ~/.zshrc
