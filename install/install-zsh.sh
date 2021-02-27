59a4c600e48c3cae8235e80602aa865299747f97
jiejie <jiejietop@gmail.com>
2021-02-27 16:39:12 +0800

    feat : add calc plugin

--- install/install-zsh.sh
+++ install/install-zsh.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+###
+ # @Author: jiejie
+ # @GitHub: https://github.com/jiejieTop
+ # @Date: 2021-02-25 15:04:20
+ # @LastEditors: jiejie
+ # @LastEditTime: 2021-02-27 13:49:35
+ # @Description: the code belongs to jiejie, please keep the author information and source code according to the license.
+### 
 
 sudo apt-get -y install git wget zsh
 
@@ -14,6 +22,11 @@ git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-
 
 git clone https://github.com/paulirish/git-open.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/git-open
 
+git clone https://github.com/arzzen/calc.plugin.zsh.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/calc/calc.plugin.zsh
+
 sed -i 's/plugins=(git)/plugins=(z git sudo extract vi-mode git-open safe-paste command-not-found colored-man-pages zsh-syntax-highlighting vscode zsh-autosuggestions)/g' ~/.zshrc
+
+source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/calc/calc.plugin.zsh/calc.plugin.zsh
+
 
 # source ~/.zshrc
